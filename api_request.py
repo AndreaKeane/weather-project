2dea1f77eb55d3b85b224a53cc11b041322aecec
AndreaKeane <alivingston1116@gmail.com>
2019-09-18 21:53:47 -0500

    iterate over a range of dates and save JSON results to MySQL database

--- api_request.py
+++ api_request.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 from configparser import ConfigParser
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 import requests
+import sqlalchemy as sa
+import json 
 
 def main():
     ''' '''
@@ -9,33 +11,72 @@ def main():
     config = ConfigParser()
     config.read('private_config.ini')
 
-    # Setup API Request
-    api_key = config['darksky']['api_key']
-    latitude = '43.0972'
-    longitude = '89.5043'
-
-    response = make_request(api_key, latitude, longitude)
-    print_response(response)
-
+    # Connect to MySQL database, with parameters provided in config
+    connection = get_db_connection(config)
+    start_date = date(2018, 1, 1)
+    end_date = date.today()
+    for single_date in daterange(start_date, end_date):
+        response = make_request(api_key=config['darksky']['api_key'], 
+                            latitude='43.0972', 
+                            longitude='89.5043', 
+                            connection=connection,
+                            time=single_date.strftime("%Y-%m-%dT00:00:00"),
+                            )
+   
     
-def make_request(api_key, latitude, longitude):
-    ''' ''' 
+def make_request(api_key, latitude, longitude, time=None, connection=None):
+    ''' 
+    Setup API Request, https://darksky.net/dev/docs
+        https://api.darksky.net/forecast/[key]/[latitude],[longitude],[time]
+        The latitude of a location (in decimal degrees). Positive is north, negative is south.
+        The longitude of a location (in decimal degrees). Positive is east, negative is west.
+        Either be a UNIX time or a string formatted
+    ''' 
     # Make Request
-    response = requests.get(f'https://api.darksky.net/forecast/{api_key}/{latitude},{longitude}')
+    if time: 
+        url_template = f'https://api.darksky.net/forecast/{api_key}/{latitude},{longitude},{time}'
+    else: 
+        url_template = f'https://api.darksky.net/forecast/{api_key}/{latitude},{longitude}'
+    
+    # Make request
+    response = requests.get(url_template)
 
     # Handle Response 
     # TODO: something more useful, raise errors
     if response.status_code == 200:
-        print('Success!')
+        # print('Success!')
+        if connection: 
+            # Retrieve the json part of the response
+            # The default string interpolation returns single-quote JSON, MySQL needs double-quotes in the JSON string
+            json_result = json.dumps(response.json())
+            connection.execute(f"REPLACE INTO requests (latitude, longitude, time, response) VALUES ({latitude}, {longitude}, NULLIF('{time}', 'None'), '{json_result}');")
     else: 
         print("ERROR")
         print(response.status_code)
     return response
 
-def print_response(response):
-    '''Temporary method to re-create original behavior''' 
-    for item in response: 
-        print(item)
+
+def get_db_connection(config):
+    '''Create and return a connection to the database specified by config'''
+     # Setup database connection 
+    user = config['database']['user']
+    pswd = ''
+    host = config['database']['host']
+    port = config['database']['port']
+    db = config['database']['db']
+
+    engine = sa.create_engine(f'mysql+mysqlconnector://{user}:{pswd}@{host}:{port}/{db}')
+    return engine.connect()
+
+
+def daterange(start_date, end_date):
+    '''
+    Generator function to iterate over a range of dates
+    Ref: https://stackoverflow.com/questions/1060279/iterating-through-a-range-of-dates-in-python
+    '''
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
 
 if __name__ == "__main__":
     main()
